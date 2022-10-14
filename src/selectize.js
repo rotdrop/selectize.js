e0e22c161a04f951ac7afacf7579a0445a4fd2c1
Claus-Justus Heine <himself@claus-justus-heine.de>
2022-10-14 22:08:12 +0200

    Attach a defined clas to the $control_input
    Rationale:
    
    Selectize likes to remove the "required" attribute from the original
    HTML element. This causes problems when one tries to style :invalid
    elements:
    
    - the original tooltip is attached to the .selectize-control element
    - the required attribute however is moved to $control_input element (a
      child of the .selectize-input div)
    - attaching a dedicated class to the actual <input... element gives the
      "consumer" applications a chance to react (style ...) required input
      element to their liking.

--- src/selectize.js
+++ src/selectize.js
@@ -133,9 +133,9 @@ $.extend(Selectize.prototype, {
 		inputMode         = self.settings.mode;
 		classes           = $input.attr('class') || '';
 
-    $wrapper          = $('<div>').addClass(settings.wrapperClass).addClass(classes + ' selectize-control').addClass(inputMode);
+		$wrapper          = $('<div>').addClass(settings.wrapperClass).addClass(classes + ' selectize-control').addClass(inputMode);
 		$control          = $('<div>').addClass(settings.inputClass + ' selectize-input items').appendTo($wrapper);
-		$control_input    = $('<input type="text" autocomplete="new-password" autofill="no" />').appendTo($control).attr('tabindex', $input.is(':disabled') ? '-1' : self.tabIndex);
+		$control_input    = $('<input type="text" autocomplete="new-password" autofill="no" />').addClass(settings.inputClass + '-element').appendTo($control).attr('tabindex', $input.is(':disabled') ? '-1' : self.tabIndex);
 		$dropdown_parent  = $(settings.dropdownParent || $wrapper);
 		$dropdown         = $('<div>').addClass(settings.dropdownClass).addClass(inputMode + ' selectize-dropdown').hide().appendTo($dropdown_parent);
 		$dropdown_content = $('<div>').addClass(settings.dropdownContentClass + ' selectize-dropdown-content').attr('tabindex', '-1').appendTo($dropdown);
